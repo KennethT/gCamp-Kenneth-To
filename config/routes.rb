572a4ee7ed0e7a681d4110bfafb12153904d217f
Kenneth To <kennethkto@gmail.com>
2015-01-26 14:48:17 -0700

    add scaffold for CRUD tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :tasks
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -8,6 +10,7 @@ Rails.application.routes.draw do
   get 'faq' => 'faq#index'
   get 'about' => 'about#index'
   get 'terms'=> 'terms#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
