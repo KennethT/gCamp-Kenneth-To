a640ad553cdd4ee844a1212f722e44542a7d09fb
Kenneth To <kennethkto@gmail.com>
2015-02-05 16:44:43 -0700

    begin projects in gCamp

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :tasks, :users
+  resources :tasks, :users, :projects
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get 'faq' => 'faq#index'
   get 'about' => 'about#index'
   get 'terms'=> 'terms#index'
+  get 'project' => 'project#index'
 
 
   # Example of regular route:
