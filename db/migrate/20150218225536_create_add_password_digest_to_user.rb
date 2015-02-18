9c333c31656bce0632cfc5cef4f1bb2534a6fe60
Kenneth To <kennethkto@gmail.com>
2015-02-18 15:59:58 -0700

    add password digest

---
+++ db/migrate/20150218225536_create_add_password_digest_to_user.rb
@@ -0,0 +1,5 @@
+class CreateAddPasswordDigestToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :password_digest, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150205231801) do
+ActiveRecord::Schema.define(version: 20150218225536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150205231801) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
+    t.string "password_digest"
   end
 
 end
