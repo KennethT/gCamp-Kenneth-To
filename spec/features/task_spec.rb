aaf4d9a4ae119d9f1c62fe9519b8bfdd8b4dc450
Kenneth To <kennethkto@gmail.com>
2015-02-18 15:09:50 -0700

    add capybara test for editing task

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -27,12 +27,29 @@ describe 'User can CRUD tasks' do
   #create task for events to appear in
   scenario 'User can view a show page for a task' do
 
-      @task = Task.create(description: "Cow", date: "2014-01-01")
-      visit "/tasks/#{@task.id}"
+    @task = Task.create(description: "Cow", date: "2014-01-01")
+    visit "/tasks/#{@task.id}"
 
-      #expect to see flash notice of successfully creation of event
-      expect(page).to have_content("Cow")
-    end
+    #expect to see flash notice of successfully creation of event
+    expect(page).to have_content("Cow")
+  end
 
+  #Users can edit an task
+  scenario 'User can edit an task' do
+    @task = Task.create(description: "Cow", date: "2014-01-01")
+    visit "/tasks/#{@task.id}/edit"
+
+    #filling out form for task
+    fill_in 'task[description]', :with => "Dog"
+
+    #submitting form to create a task
+    click_on "Create Task"
+
+    #now we expect the index page to have the description of the task we created.
+    expect(page).to have_content("Dog")
+    # expecting the flash notification
+    expect(page).to have_content("Task was successfully updated")
+    
+  end
 
 end
