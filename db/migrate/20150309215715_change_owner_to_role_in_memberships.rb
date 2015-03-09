95d7cc727c58398e3ffd5ef1cbccdf2ea1425198
Kenneth To <kennethkto@gmail.com>
2015-03-09 16:02:16 -0600

    remove owner column, add role column in membership table

---
+++ db/migrate/20150309215715_change_owner_to_role_in_memberships.rb
@@ -0,0 +1,8 @@
+class ChangeOwnerToRoleInMemberships < ActiveRecord::Migration
+
+  def change
+    remove_column(:memberships,:owner,:boolean)
+
+    add_column(:memberships,:role,:integer)
+  end
+end
