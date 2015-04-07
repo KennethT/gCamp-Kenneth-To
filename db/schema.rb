7bc8886cccb40b0021ae6c9c97391032640c6cf4
Kenneth To <kennethkto@gmail.com>
2015-04-07 14:06:29 -0600

    add comment count to task

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309215715) do
+ActiveRecord::Schema.define(version: 20150407200410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string  "comment"
+    t.integer "task_id"
+  end
+
+  add_index "comments", ["task_id"], name: "index_comments_on_task_id", using: :btree
 
   create_table "memberships", force: :cascade do |t|
     t.integer "project_id"
@@ -49,6 +56,7 @@ ActiveRecord::Schema.define(version: 20150309215715) do
     t.string "password_digest"
   end
 
+  add_foreign_key "comments", "tasks"
   add_foreign_key "memberships", "projects"
   add_foreign_key "memberships", "users"
   add_foreign_key "tasks", "projects"
