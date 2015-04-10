2b2bab4436b321b5e2a5c858942f4cc732744927
Kenneth To <kennethkto@gmail.com>
2015-04-10 13:52:49 -0600

    add table counts to about view page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,11 @@
 class AboutController < ApplicationController
   def index
+    # @project = Project.find(params[:project_id])
+    # @task = Task.find(params[:task_id])
+    @projects = Project.all
+    @tasks = Task.all
+    @memberships = Membership.all
+    @users = User.all
+    @comments = Comment.all
   end
 end
