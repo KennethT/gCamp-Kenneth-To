553982171612096659d895ceeda59c1cd5dad3ed
Kenneth To <kennethkto@gmail.com>
2015-04-17 09:55:40 -0600

    add admin to params of user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,7 +56,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :admin)
   end
 
 
