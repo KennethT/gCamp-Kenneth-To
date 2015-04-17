c11a59a49ab4d32ebb084b9bef834cd36f376996
Kenneth To <kennethkto@gmail.com>
2015-04-17 10:01:52 -0600

    add 404 for user showpage if not current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if current_user != @user
+      render :file => 'public/404.html', :status => :not_found, :layout => false
+    end
   end
 
   def edit
